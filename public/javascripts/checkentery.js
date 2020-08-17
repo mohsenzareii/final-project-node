65d8e3a247a3f8064be07c35362da9202577f408
mohsenzareii <mohsenzarei291@gmail.com>
2020-08-17 17:28:26 +0430

    add user model and add new user to database

--- public/javascripts/checkentery.js
+++ public/javascripts/checkentery.js
@@ -4,32 +4,39 @@ $(document).ready(function(){
         let id = $(this).attr('id');
         switch(id){
             case 'userName' :
-                if(value.length < 8 || value.length > 30){
+                if(value.length < 3 || value.length > 30){
                     $('#userNameHelp').html('تعداد کاراکتر ها حداقل 8 و حداکثر 30 باشد!');
                 } else {
-                    $('#userNameHelp').html('');
+                    $('#userNameHelp').html('*الزامی');
                 }
                 break;
             case 'password' :
                 if(value.length <8 || value.length > 30){
                     $('#passwordHelp').html('تعداد کاراکتر ها حداقل 8 و حداکثر 30 باشد!');
                 } else {
-                    $('#passwordHelp').html('');
+                    $('#passwordHelp').html('*الزامی');
                 }
                 break;
             case 'firstName' :
-                if(value.length <8 || value.length > 30){
+                if(value.length <3 || value.length > 30){
                     $('#firstNameHelp').html('تعداد کاراکتر ها حداقل 8 و حداکثر 30 باشد!');
                 } else {
-                    $('#firstNameHelp').html('');
+                    $('#firstNameHelp').html('*الزامی');
                 }  
                 break;
             case 'lastName' :
-                if(value.length <8 || value.length > 30){
+                if(value.length <3 || value.length > 30){
                     $('#lastNameHelp').html('تعداد کاراکتر ها حداقل 8 و حداکثر 30 باشد!');
                 } else {
-                    $('#lastNameHelp').html('');
-                }      
+                    $('#lastNameHelp').html('*الزامی');
+                }  
+                break;
+            case 'mobile' : 
+                if(value.length != 11 || value.length !=10){
+                    $('#mobileHelp').html('شماره نامعتبر است!');
+                } else {
+                    $('#mobileHelp').html('*الزامی');
+                }       
         }
     });
 });
