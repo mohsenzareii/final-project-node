ec3ed5010d768ae640c46f962f74c24a75a89379
mohsenzareii <mohsenzarei291@gmail.com>
2020-08-21 18:52:49 +0430

    add session

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
-const { findById } = require('../models/user');
 
 
 /* GET users listing. */
 router.get('/dashboard', async(req, res) => {
 
   
-  res.render('pages/dashboard');
+  res.render('pages/dashboard', {user : req.session.user});
 });
 
 module.exports = router;
