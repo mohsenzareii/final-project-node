23a2f8f699ae026800d29fa44d8c4bd9fbce1842
mohsenzareii <mohsenzarei291@gmail.com>
2020-08-21 18:07:37 +0430

    add dashboard

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
+const { findById } = require('../models/user');
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/dashboard', async(req, res) => {
+
+  
+  res.render('pages/dashboard');
 });
 
 module.exports = router;
