3e537242e6890505d8dc8f18f2fcd9e0e11ba95e
mohsenzareii <mohsenzarei291@gmail.com>
2020-08-23 17:12:06 +0430

    check session

--- routes/api.js
+++ routes/api.js
@@ -4,13 +4,25 @@ const loginRoute = require('../routes/login');
 const signupRoute = require('../routes/signup');
 const userRoute = require('../routes/users');
 
+//check session for login 
+const isLogin = function (req, res, next){
+    if(req.session.user) return res.redirect('/api/users/dashboard');
+    next();
+}
+
+//check session for access to dashboard 
+const checkSession = function(req, res, next){
+    if(!req.session.user) return res.redirect('/api/login');
+    next();
+}
+
 //route to login.js
-router.use('/login', loginRoute);
+router.use('/login', isLogin, loginRoute);
 
 //route to signup.js
 router.use('/signup', signupRoute);
 
 //route to dashboard.js
-router.use('/users', userRoute);
+router.use('/users', checkSession, userRoute);
 
 module.exports = router;
